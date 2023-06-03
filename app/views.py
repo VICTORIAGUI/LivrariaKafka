591ffa5d101e267c62d6aa08a26170adcb48a3a0
VICTORIAGUI <victoriagui.santos@gmail.com>
2023-06-03 13:59:43 -0300

    att traço

--- app/views.py
+++ app/views.py
@@ -4,16 +4,16 @@ from django.shortcuts import render
 
 def home(request): #Função para chamar a home
     return render(request,
-     'app\home.html')
+     'app/home.html')
 
 def ebook(request): #Função para chamar a página ebook
     return render(request,
-     'app\ebook.html')
+     'app/ebook.html')
 
 def infantil(request): #Função para chamar a página infantil
     return render(request,
-    'app\infantil.html')
+    'app/infantil.html')
 
 def importados(request): #Função para chamar a página importados
     return render(request,
-    'app\importados.html')
+    'app/importados.html')
