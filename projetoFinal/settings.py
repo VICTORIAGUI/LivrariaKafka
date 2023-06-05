74791641a076b25ce13e44b497e4b4dc416fc02b
VICTORIAGUI <victoriagui.santos@gmail.com>
2023-06-05 17:22:43 -0300

    deploy

--- projetoFinal/settings.py
+++ projetoFinal/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'assets' / 'static'
 ]
